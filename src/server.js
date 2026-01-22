6c1258ccb2bda35811294c2d4b1a95146eb3c0a5
gabrielrodrigueslb <gbri2586@gmail.com>
2026-01-22 16:38:45 -0300

    update in  database

--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
 import express from 'express'
-import {env} from './config/env.js'
+import cors from 'cors'
+import { env } from './config/env.js'
+
 import healthRoute from './routes/health.js'
 import unidadesRoute from './routes/unidades.js'
 import produtosRoute from './routes/produtos.js'
 
 const app = express()
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173', // Vite
+    'http://localhost:3000', // se usar outro front
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json())
 
-app.use('/health', healthRoute);
+app.use('/health', healthRoute)
 app.use('/unidades', unidadesRoute)
 app.use('/produtos', produtosRoute)
 
-console.log('PWD LEN:', process.env.DB_PASSWORD.length)
-
-
 app.listen(env.port, () => {
-    console.log(`°Api rodando na porta ${env.port}°`)
+  console.log(`°Api rodando na porta ${env.port}°`)
 })
