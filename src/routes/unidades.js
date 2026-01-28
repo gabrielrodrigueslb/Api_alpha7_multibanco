b962c91ddfed512f0181942e6da0c6ff9d053920
gabrielrodrigueslb <gbri2586@gmail.com>
2026-01-28 12:05:52 -0300

    Fix: puxar apenas lojas com estoque

--- src/routes/unidades.js
+++ src/routes/unidades.js
@@ -1,26 +1,34 @@
-import {Router} from 'express';
-import {pool} from '../db.js'
+import { Router } from 'express';
+import { pool } from '../db.js';
 
 const router = Router();
 
 /* GET /unidades
-Lista unidades de negócio ativas */
+   Lista unidades de negócio ativas QUE POSSUEM ESTOQUE */
 
 router.get('/', async (req, res) => {
-    try{
+    try {
         const result = await pool.query(`
-            SELECT id, codigo, nome, nomefantasia, estado, cidade FROM unidadenegocio WHERE status = 'A' ORDER BY nome
-            `);
+            SELECT u.id, u.codigo, u.nome, u.nomefantasia, u.estado, u.cidade 
+            FROM unidadenegocio u 
+            WHERE u.status = 'A' 
+            AND EXISTS (
+                SELECT 1 
+                FROM estoque e 
+                WHERE e.unidadenegocioid = u.id 
+                AND e.estoque > 0
+            )
+            ORDER BY u.nome
+        `);
 
-            res.json({total: result.rowCount,
-                data:result.rows
-            });
-    } catch(error) {
-        console.error('Erro ao buscar uniddes', error);
-        res.status(500).json({error: 'Erro ao buscar unidades'})
-        
+        res.json({
+            total: result.rowCount,
+            data: result.rows
+        });
+    } catch (error) {
+        console.error('Erro ao buscar unidades:', error);
+        res.status(500).json({ error: 'Erro ao buscar unidades' });
     }
-})
+});
 
-
-export default router
+export default router;
